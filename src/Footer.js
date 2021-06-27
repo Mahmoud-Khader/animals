cca0d8e8c711883056f8cec8b0b7c4aa9a77c9c8
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-06-27 18:17:52 +0300

    finished

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                <footer>The Author is Mahmoud Khader</footer>
+            </div>
+        )
+    }
+}
+
+export default Footer
---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+import HornedBeast1 from './HornedBeast1';
+
+class Main extends React.Component{
+    render(){
+        return(
+            <div>
+                <HornedBeast/>
+                <HornedBeast1/>
+            </div>
+        )
+    }
+}
+
+export default Main
