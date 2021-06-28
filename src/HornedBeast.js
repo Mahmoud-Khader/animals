64d9eaf16a534dfaed3ee5e53f17c2806c7b4053
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-06-28 14:05:46 +0300

    fixed the lab

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class HornedBeast extends React.Component{
     render(){
+
         return(
             <div>
-                <h2>UniWhal</h2>
-                <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"alt="" />
-                <p>A unicorn and a narwhal nuzzling their horns</p>
+                
+                <h2>{this.props.title}</h2>
+                <img src={this.props.image_url}alt={this.props.alt} />
+                <p>{this.props.description}</p>
             </div>
         )
     }
